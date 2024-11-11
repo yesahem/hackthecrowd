72b1983c0bac073b3722140f76b69f763b54da1f
yesahem <ahemraj82@gmail.com>
2024-11-12 01:43:37 +0530

    testing final

--- utils.ts
+++ utils.ts
@@ -1,7 +1,9 @@
 export default function getApiUrl() {
     const environment = process.env.VERCEL_ENV!;
+    console.log(environment);
 
     if (environment === "Production") {
+        
         return `https://${process.env.VERCEL_URL!}`;
     } else {
         return process.env.NEXT_PUBLIC_API_URL!;
